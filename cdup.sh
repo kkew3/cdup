a658ee181af923faace11ee9587480e20b801830
Kaiwen Wu <kps6326@hotmail.com>
2018-08-23 15:24:25 -0700

    Added error handling

--- cdup.sh
+++ cdup.sh
@@ -6,23 +6,32 @@ up() {
 
 	local parent="$1"
 	local todir=""
+	local errno=0
 	if [ -z "$parent" ]; then
 		todir=".."
 	else
 		local levelup=""
 		if [ "${parent:0:1}" = "/" ]; then
+			if ! "$parentlevel" -e "${parent#?}" > /dev/null; then errno=1; fi
 			levelup=$("$parentlevel" -e "${parent#?}")
 		else
+			if ! "$parentlevel" "${parent}" > /dev/null; then errno=1; fi
 			levelup=$("$parentlevel" "${parent}")
 		fi
-		if [ "$levelup" -gt 0 ]; then
-			todir="."
-			for ((n=0;n<$levelup;n++)); do
-				todir="$todir/.."
-			done
+		if [ "$errno" = 0 ]; then
+			if [ "$levelup" -gt 0 ]; then
+				todir="."
+				for ((n=0;n<$levelup;n++)); do
+					todir="$todir/.."
+				done
+			fi
 		fi
 	fi
-	if [ ! -z "$todir" ]; then
-		cd "$todir"
+	if [ "$errno" = 0 ]; then
+		if [ ! -z "$todir" ]; then
+			cd "$todir"
+		fi
+	else
+		return "$errno"
 	fi
 }
