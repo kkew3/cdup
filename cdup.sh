e788e890527250f701c1fce66ffd894001244281
kkew3 <kps6326@hotmail.com>
2018-12-30 19:34:52 -0800

    Added support for cd immediately after jumping upward

--- cdup.sh
+++ cdup.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
-__up_help() 
-{ 
+__up_help ()
+{
     cat  <<EOF
 Usage: up                   Same as \`cd ..'
        up -n LEVEL          Same as \`cd ../.. etc.' (LEVEL \`..'s)
        up NAME              Go upwards to the nearest directory named NAME
        up /REGEX/           Go upwards to the nearest directory matching REGEX
                             (grep-style regex)
+       ... [-s DIR]         Any command pattern above plus this option will
+                            go up to the underlying ancestor directory before
+                            going downwards to DIR, such that there's only one
+                            \`cd' action
        up [-h | --help]     Show this help and exit
 EOF
 }
 
-up() 
-{ 
+
+up ()
+{
     local uplevel=;
     local todirname=;
     local todirpattern=;
     local xpat=;
     local todir=;
+    local subdir=;
     while [ -n "$1" ]; do
-        if [ "$1" = "-n" ]; then
-            shift;
-            if [ -z "$1" ]; then
-                __up_help;
-                return 1;
-            else
-                uplevel="$1";
+        if [ "${1::2}" = "-n" ]; then
+            uplevel="${1:2}";
+            case "$uplevel" in
+                '' | *[!0-9]*)
+                    uplevel=
+                ;;
+                *)
+                    :
+                ;;
+            esac;
+            if [ -z "$uplevel" ]; then
+                shift;
+                if [ -z "$1" ]; then
+                    __up_help;
+                    return 1;
+                else
+                    uplevel="$1";
+                fi;
             fi;
         else
             if [ "$1" = "-h" -o "$1" = "--help" ]; then
@@ -36,14 +53,28 @@ up()
                 if [ "$1" = "-E" ]; then
                     xpat="-E";
                 else
-                    case "$1" in 
-                        /*/)
-                            todirpattern="$1"
-                        ;;
-                        *)
-                            todirname="$1"
-                        ;;
-                    esac;
+                    if [ "${1::2}" = "-s" ]; then
+                        if [ -z "${1:2}" ]; then
+                            shift;
+                            if [ -z "$1" ]; then
+                                __up_help;
+                                return 1;
+                            else
+                                subdir="$1";
+                            fi;
+                        else
+                            subdir="${1:2}";
+                        fi;
+                    else
+                        case "$1" in
+                            /*/)
+                                todirpattern="$1"
+                            ;;
+                            *)
+                                todirname="$1"
+                            ;;
+                        esac;
+                    fi;
                 fi;
             fi;
         fi;
@@ -53,20 +84,19 @@ up()
         uplevel=1;
     fi;
     if [ -n "$uplevel" ]; then
-        if echo "$uplevel" | grep -q '^+\?[0-9]\+$'; then
-            if [ "$uplevel" -gt 0 ]; then
-                todir="$(pwd)";
-                while [ "$uplevel" -gt 0 ]; do
-                    todir="$(dirname "$todir")";
-                    uplevel="$(( "$uplevel" - 1 ))";
-                done;
-                cd "$todir";
+        if [ "$uplevel" -gt 0 ]; then
+            todir="$(pwd)";
+            while [ "$uplevel" -gt 0 ]; do
+                todir="$(dirname "$todir")";
+                uplevel="$(( "$uplevel" - 1 ))";
+            done;
+            if [ -n "$subdir" ]; then
+                cd "$todir/$subdir";
             else
-                return 0;
+                cd "$todir";
             fi;
         else
-            echo LEVEL should be nonnegative integer >> /dev/stderr;
-            return 1;
+            return 0;
         fi;
     else
         if [ -n "$todirname" ]; then
@@ -81,7 +111,11 @@ up()
                 echo Nothing matches NAME \"$todirname\" >> /dev/stderr;
                 return 4;
             else
-                cd "$todir";
+                if [ -n "$subdir" ]; then
+                    cd "$todir/$subdir";
+                else
+                    cd "$todir";
+                fi;
             fi;
         else
             if [ -n "$todirpattern" ]; then
@@ -97,7 +131,11 @@ up()
                     echo Nothing matches REGEX \'$todirpattern\' >> /dev/stderr;
                     return 4;
                 else
-                    cd "$todir";
+                    if [ -n "$subdir" ]; then
+                        cd "$todir/$subdir";
+                    else
+                        cd "$todir";
+                    fi;
                 fi;
             fi;
         fi;
