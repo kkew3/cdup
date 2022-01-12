1083abe63626e98a0990c2ad9299118008302e40
kkew3 <kps6326@hotmail.com>
2022-01-12 11:54:52 +0800

    Made full command line argument parser with tests

--- cdup_rs_backend/src/main.rs
+++ cdup_rs_backend/src/main.rs
@@ -13,60 +13,208 @@ enum ErrorType {
     InvalidUnicode,
 }
 
-struct Cli {
+enum RuleType {
+    N,
+    Raw,
+    Glob,
+    Regex,
+}
+
+struct Cli<'a> {
     fromdir: PathBuf,
-    rule_type: String,
-    rule_value: String,
-    subsequent_dir: Option<String>,
+    rule_type: RuleType,
+    rule_value: &'a str,
+    subsequent_dir: Option<&'a str>,
+    list: bool,
 }
 
 fn main() {
-    let mut cli = parse_args(&mut env::args());
+    let args: Vec<String> = env::args().collect();
+    let mut cli = parse_args(&args);
+    let cwd = cli.fromdir.clone();
 
-    if cli.rule_type == "n" {
-        handle_n(&mut cli.fromdir, &cli.rule_value);
-    } else if cli.rule_type == "raw" {
-        handle_raw(&mut cli.fromdir, &cli.rule_value);
-    } else if cli.rule_type == "glob" {
-        handle_glob(&mut cli.fromdir, &cli.rule_value);
-    } else if cli.rule_type == "regex" {
-        handle_regex(&mut cli.fromdir, &cli.rule_value);
-    } else {
-        eprintln!("up: invalid rule type");
-        process::exit(ERROR_ARGS);
+    match cli.rule_type {
+        RuleType::N => handle_n(&mut cli.fromdir, cli.rule_value),
+        RuleType::Raw => handle_raw(&mut cli.fromdir, cli.rule_value),
+        RuleType::Glob => handle_glob(&mut cli.fromdir, cli.rule_value),
+        RuleType::Regex => handle_regex(&mut cli.fromdir, cli.rule_value),
     }
 
     if let Some(d) = cli.subsequent_dir {
         cli.fromdir.push(d);
     }
+
     match cli.fromdir.to_str() {
         None => {
             eprintln!("up: invalid Unicode in {:?}", cli.fromdir);
             process::exit(ERROR_ARGS);
         }
-        Some(s) => print!("{}", s),
+        Some(s) => {
+            if cli.list {
+                println!("echo \"{}\"", s);
+            } else if cli.fromdir != cwd {
+                println!("cd \"{}\"", s);
+            }
+        }
     }
 }
 
-fn parse_args(args_iter: &mut env::Args) -> Cli {
-    let _bin = args_iter.next();
-    // assuming absolute directory
-    let fromdir = PathBuf::from(args_iter.next().unwrap());
-    // assuming one of ["n", "raw", "glob", "regex"]
-    let rule_type = args_iter.next().unwrap();
-    // assuming parseable as usize if rule_type == "n"
-    let rule_value = args_iter.next().unwrap();
-    let subsequent_dir = args_iter.next().unwrap();
-    let subsequent_dir = match &subsequent_dir[..] {
-        "" => None,
-        s => Some(String::from(s)),
-    };
+fn parse_args(args: &Vec<String>) -> Cli {
+    let fromdir = PathBuf::from(args[1].clone());
+    let mut subsequent_dir: Option<&str> = None;
+    let mut list = false;
+    let mut rule_type: Option<RuleType> = None;
+    let mut rule_value: Option<&str> = None;
+
+    let mut rule_begin = false;
+    // set to true if one of the OPTIONS or `--' has been parsed in current
+    // loop
+    let mut option_parsed: bool;
+    let n = args.len();
+    let mut cur: usize = 2;
+
+    while cur < n {
+        option_parsed = false;
+        if !rule_begin {
+            if args[cur] == "-h" || args[cur] == "--help" {
+                println!(
+                    "\
+usage: up [OPTIONS...] [[--] UPWARD_RULE]
+
+OPTIONS
+
+    -h, --help              Show this help and return 0
+    -s DIR                  Going downwards to DIR after going upwards, such
+                            that there's only one `cd' action in total
+    -l                      Print the absolute target directory rather than
+                            actually cd to it; the target directory will be
+                            printed regardless of its existence
+
+UPWARD_RULE
+
+    Can be one of:
+
+        <Nothing>           Same as `cd ..'
+        -NUM_LEVELS         Same as `cd ..' NUM_LEVELS time but there will be
+                            only one `cd' action in total. If NUM_LEVELS
+                            contains non-digit characters, or if NUM_LEVELS
+                            is empty, the entire `-NUM_LEVELS' will be
+                            interpreted as `NAME' (see below). If NUM_LEVELS
+                            is `0', nothing will be done
+        [-r] NAME           Go upwards to the nearest directory named NAME.
+                            The optional `-r' disambiguates cases when NAME
+                            starts with `-'
+        -g PATTERN          Go upwards to the nearest directory matching the
+                            python-style globbing pattern PATTERN. Be sure to
+                            add quote around PATTERN to avoid unnecessary
+                            shell expansion
+        -E REGEX            Go upwards to the nearest directory matching the
+                            python REGEX
+
+The order of OPTIONS and UPWARD_RULE does not matter, and can be interleaved.
+The optional `--' marks the beginning of UPWARD_RULE. Short options cannot
+be merged together. Option with argument can be merged together. No
+UPWARD_RULE other than `-NUM_LEVELS' is allowed to reach the root
+directory (`/').
+
+Error code
+
+    0                       Successs
+    1                       cd error (`No such file or directory'). This
+                            error is most often triggered by `-s' option as
+                            unable to target directory upward will lead to
+                            return code 4
+    2                       Cmd argument error
+    4                       Cannot find the target directory upward"
+                );
+                process::exit(0);
+            }
+            if args[cur].starts_with("-s") {
+                if args[cur].len() > 2 {
+                    subsequent_dir = Some(&args[cur][2..]);
+                } else if cur + 1 < n {
+                    subsequent_dir = Some(&args[cur + 1]);
+                    cur += 1;
+                } else {
+                    eprintln!("DIR missing");
+                    process::exit(ERROR_ARGS);
+                }
+                option_parsed = true;
+            } else if args[cur] == "-l" {
+                list = true;
+                option_parsed = true;
+            } else if args[cur] == "--" {
+                rule_begin = true;
+                option_parsed = true;
+            }
+        }
+        if !option_parsed {
+            if let Some(_) = rule_type {
+                eprintln!("UPWARD_RULE has already been specified");
+                process::exit(ERROR_ARGS);
+            }
+            if args[cur].starts_with("-r") {
+                rule_type = Some(RuleType::Raw);
+                if args[cur].len() > 2 {
+                    rule_value = Some(&args[cur][2..]);
+                } else if cur + 1 < n {
+                    rule_value = Some(&args[cur + 1]);
+                    cur += 1;
+                } else {
+                    eprintln!("NAME missing");
+                    process::exit(ERROR_ARGS);
+                }
+            } else if args[cur].starts_with("-g") {
+                rule_type = Some(RuleType::Glob);
+                if args[cur].len() > 2 {
+                    rule_value = Some(&args[cur][2..]);
+                } else if cur + 1 < n {
+                    rule_value = Some(&args[cur + 1]);
+                    cur += 1;
+                } else {
+                    eprintln!("PATTERN missing");
+                    process::exit(ERROR_ARGS);
+                }
+            } else if args[cur].starts_with("-E") {
+                rule_type = Some(RuleType::Regex);
+                if args[cur].len() > 2 {
+                    rule_value = Some(&args[cur][2..]);
+                } else if cur + 1 < n {
+                    rule_value = Some(&args[cur + 1]);
+                    cur += 1;
+                } else {
+                    eprintln!("REGEX missing");
+                    process::exit(ERROR_ARGS);
+                }
+            } else if args[cur].starts_with("-") {
+                if args[cur].len() > 1
+                    && args[cur][1..].chars().all(|c| c.is_ascii_digit())
+                {
+                    rule_type = Some(RuleType::N);
+                    rule_value = Some(&args[cur][1..]);
+                } else {
+                    rule_type = Some(RuleType::Raw);
+                    rule_value = Some(&args[cur]);
+                }
+            } else {
+                rule_type = Some(RuleType::Raw);
+                rule_value = Some(&args[cur]);
+            }
+        }
+        cur += 1;
+    }
+
+    if let None = rule_type {
+        rule_type = Some(RuleType::N);
+        rule_value = Some("1");
+    }
 
     Cli {
         fromdir,
-        rule_type,
-        rule_value,
+        rule_type: rule_type.unwrap(),
+        rule_value: rule_value.unwrap(),
         subsequent_dir,
+        list,
     }
 }
 
@@ -84,7 +232,7 @@ fn handle_raw(fromdir: &mut PathBuf, rule_value: &str) {
         Err(ErrorType::InvalidUnicode) => {
             // this arm shouldn't be reached since raw_search_upward should not
             // return InvalidUnicode
-            eprintln!("up: invalid Unicode in \"{:?}\"", fromdir);
+            eprintln!("up: invalid Unicode in {:?}", fromdir);
             process::exit(ERROR_ARGS);
         }
     }
@@ -105,7 +253,7 @@ fn handle_glob(fromdir: &mut PathBuf, rule_value: &str) {
             process::exit(ERROR_NOMATCH);
         }
         Err(ErrorType::InvalidUnicode) => {
-            eprintln!("up: invalid Unicode in \"{:?}\"", fromdir);
+            eprintln!("up: invalid Unicode in {:?}", fromdir);
             process::exit(ERROR_ARGS);
         }
     }
@@ -126,7 +274,7 @@ fn handle_regex(fromdir: &mut PathBuf, rule_value: &str) {
             process::exit(ERROR_NOMATCH);
         }
         Err(ErrorType::InvalidUnicode) => {
-            eprintln!("up: invalid Unicode in \"{:?}\"", fromdir);
+            eprintln!("up: invalid Unicode in {:?}", fromdir);
             process::exit(ERROR_ARGS);
         }
     }
@@ -213,6 +361,83 @@ fn regex_search_upward(
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_parse_args() {
+        let args = vec![String::from("bin"), String::from("/hello/world")];
+        let cli = parse_args(&args);
+        assert_eq!(cli.fromdir, PathBuf::from("/hello/world"));
+        assert!(matches!(cli.subsequent_dir, None));
+        assert_eq!(cli.list, false);
+        assert!(matches!(cli.rule_type, RuleType::N));
+        assert_eq!(cli.rule_value, "1");
+
+        let args = vec![
+            String::from("bin"),
+            String::from("/hello/world"),
+            String::from("-gP*s"),
+        ];
+        let cli = parse_args(&args);
+        assert_eq!(cli.fromdir, PathBuf::from("/hello/world"));
+        assert!(matches!(cli.subsequent_dir, None));
+        assert_eq!(cli.list, false);
+        assert!(matches!(cli.rule_type, RuleType::Glob));
+        assert_eq!(cli.rule_value, "P*s");
+
+        let args = vec![
+            String::from("bin"),
+            String::from("/hello/world"),
+            String::from("-E"),
+            String::from("P"),
+            String::from("-sagain"),
+            String::from("-l"),
+        ];
+        let cli = parse_args(&args);
+        assert_eq!(cli.fromdir, PathBuf::from("/hello/world"));
+        assert!(matches!(cli.subsequent_dir, Some("again")));
+        assert_eq!(cli.list, true);
+        assert!(matches!(cli.rule_type, RuleType::Regex));
+        assert_eq!(cli.rule_value, "P");
+
+        let args = vec![
+            String::from("bin"),
+            String::from("/hello/world"),
+            String::from("--xxx"),
+        ];
+        let cli = parse_args(&args);
+        assert_eq!(cli.fromdir, PathBuf::from("/hello/world"));
+        assert!(matches!(cli.subsequent_dir, None));
+        assert_eq!(cli.list, false);
+        assert!(matches!(cli.rule_type, RuleType::Raw));
+        assert_eq!(cli.rule_value, "--xxx");
+
+        let args = vec![
+            String::from("bin"),
+            String::from("/hello/world"),
+            String::from("--"),
+            String::from("-s"),
+        ];
+        let cli = parse_args(&args);
+        assert_eq!(cli.fromdir, PathBuf::from("/hello/world"));
+        assert!(matches!(cli.subsequent_dir, None));
+        assert_eq!(cli.list, false);
+        assert!(matches!(cli.rule_type, RuleType::Raw));
+        assert_eq!(cli.rule_value, "-s");
+
+        let args = vec![
+            String::from("bin"),
+            String::from("/hello/world"),
+            String::from("-r-sagain"),
+            String::from("-s"),
+            String::from("from"),
+        ];
+        let cli = parse_args(&args);
+        assert_eq!(cli.fromdir, PathBuf::from("/hello/world"));
+        assert!(matches!(cli.subsequent_dir, Some("from")));
+        assert_eq!(cli.list, false);
+        assert!(matches!(cli.rule_type, RuleType::Raw));
+        assert_eq!(cli.rule_value, "-sagain");
+    }
 
     #[test]
     fn test_raw_search_upward() {
